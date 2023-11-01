381d7fdc127701c2ebc31e4dbe20714606a22b88
Ashimoanamey Joshua Hassan <ashimojoshuahassan@gmail.com>
2023-11-01 23:00:23 +0100

    commiting Dee

---
+++ README.md
@@ -0,0 +1,1 @@
+Web page for Dee Empire with image collage and hover changin buttons and links
---
+++ myindex.js
@@ -0,0 +1,21 @@
+document.getElementById("dgreen").innerHTML ="DEE's";
+document.getElementById("dgreen").style.color ="#20cb06";
+document.getElementById("dgreen").style.fontSize ="55px";
+document.getElementById("dgreen").style.fontWeight="bolder";
+
+document.getElementById("FE").innerHTML ="Fashion Empire <br>";
+document.getElementById("FE").style.color ="";
+document.getElementById("FE").style.fontSize ="55px";
+
+document.getElementById("caught").innerHTML ="Never to be caught ";
+document.getElementById("caught").style.fontSize ="110px";
+document.getElementById("caught").style.fontWeight="bolder";
+
+document.getElementById("un").innerHTML ="unfresh";
+document.getElementById("un").style.fontSize ="110px";
+document.getElementById("un").style.fontWeight="bolder";
+document.getElementById("un").style.color ="#20cb06";
+
+document.getElementById("c").innerHTML ="<sup>" + "&copy" + "</sup>";
+document.getElementById("c").style.fontSize ="55px";
+
